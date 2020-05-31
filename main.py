c95a9deac1063d0a953dd42ee6409f9032350ce0
Aleksandr Michuda <michuda@chet.ucdavis.edu>
2020-05-31 14:28:14 -0700

    added classifier for agroecological zones. changed main to compare regions and agro-ecological zones

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# %%
+#! load_ext autoreload
+
+# %%
+#! autoreload 2
 from predictor.classifier_prediction import ClassifierPredictor
 from predictor.table_predictor import TablePredictor
 
@@ -10,14 +15,16 @@ Input has to be a list.
 There is a TablePredictor and a ClassifierPredictor which should both be used for comparison.
 """
 
-t = TablePredictor()
-
+# %%
+# Get some representative names
 surnames = ['Ahimbisibwe', 'Auma', 'Amin', 'Makubuya', 'Museveni', 'Oculi', 'Kadaga']
 
+# %%
+t = TablePredictor()
 table_predict = t.predict(surnames, n_jobs=10)
-print(table_predict)
+print(table_predict.to_markdown())
 
-
+# %%
 # Regular XGB Predictor
 c = ClassifierPredictor()
 
@@ -26,5 +33,37 @@ predict_xgb = c.predict(surnames,
               predict_prob = True,
               df_out =True)
 
-print(predict_xgb)
+print(predict_xgb.to_markdown())
+
+# %%
+# Agro-Eological Zone Predictor
+cag = ClassifierPredictor(agro_eco=True)
+
+predict_xgb_agro_eco = cag.predict(surnames, 
+              get_label_names=True, 
+              predict_prob = True,
+              df_out =True)
+
+print(predict_xgb_agro_eco.to_markdown())
+
+# %%
+# Compare Regions and Agro-ecological zones
+
+## First run without probability prediction
+region_df = c.predict(surnames, 
+              get_label_names=True, 
+              predict_prob=True,
+              df_out =True)
+
+# Not predicting probabilities for agro_eco to make a smaller table
+agro_eco_df = cag.predict(surnames, 
+              get_label_names=True, 
+              df_out =True)
+
+comp_df = region_df.merge(agro_eco_df, 
+                  left_index=True,
+                  right_index = True)
+
+print(comp_df.to_markdown())
+
 
--- predictor/classifier_prediction.py
+++ predictor/classifier_prediction.py
@@ -1,17 +1,22 @@
 import joblib
 from pathlib import Path
+import xgboost as xgb
 
 import pandas as pd
 from predictor.exceptions import NoTextException
 import os
 
+
 class ClassifierPredictor:
-    
-    def __init__(self, 
-                 tfidf_path = None, 
-                 model_path = None, 
-                 label_encoder_path = None,
-                 **kwargs):
+    def __init__(
+        self,
+        tfidf_path=None,
+        model_path=None,
+        label_encoder_path=None,
+        agro_eco=False,
+        xgb_load = False,
+        **kwargs
+    ):
         """This class uses a pickled trained classifier to make predictions about surnames.
         It loads the pickled processors (which include a tfidf transformer and label encoder),
         as well as the classifier and then trainsforms input text and give prediction
@@ -20,51 +25,87 @@ class ClassifierPredictor:
             tfidf_path {str} -- path to trained tfidf transformer (joblib object) (default: {None})
             model_path {str} -- path to joblib pickle of trained model (default: {None})
             label_encoder_path {str} -- path to label encoder pickled object (default: {None})
-        """
+        """        
         
-        super().__init__(**kwargs)
-        
+        self.xgb_load = xgb_load
+
         if tfidf_path is None:
-            tfidf_path = Path("predictor","saved_models", "tfidf_multilabel_False_nokampala_True.joblib")
+            if agro_eco:
+                tfidf_path = Path(
+                    "predictor",
+                    "saved_models",
+                    "tfidf_multilabel_False_nokampala_True_agro_zone_smote_False_opt.joblib",
+                )
+
+            else:
+                tfidf_path = Path(
+                    "predictor",
+                    "saved_models",
+                    "tfidf_multilabel_False_nokampala_True.joblib",
+                )
         else:
             tfidf_path = Path(tfidf_path)
-            
+
         if label_encoder_path is None:
-            label_encoder_path = Path("predictor", 'saved_models', 'label_encoder_multilabel_False_nokampala_True.joblib')
+
+            if agro_eco:
+                label_encoder_path = Path(
+                    "predictor",
+                    "saved_models",
+                    "label_encoder_multilabel_False_nokampala_True_agro_zone_smote_False_opt.joblib",
+                )
+            else:
+                label_encoder_path = Path(
+                    "predictor",
+                    "saved_models",
+                    "label_encoder_multilabel_False_nokampala_True.joblib",
+                )
         else:
             label_encoder_path = Path(label_encoder_path)
-        
+
         if model_path is None:
-            model_path = Path("predictor","saved_models", "xgb_None_multilabel_False_add_kampala_True.joblib")
+
+            if agro_eco:
+                model_path = Path(
+                    "predictor",
+                    "saved_models",
+                    "xgb_None_multilabel_False_add_kampala_True_agro_zone_smote_False_opt.joblib",
+                )
+            else:
+                model_path = Path(
+                    "predictor",
+                    "saved_models",
+                    "xgb_None_multilabel_False_add_kampala_True.joblib",
+                )
         else:
             model_path = Path(model_path)
-            
+
         self.tfidf_path = tfidf_path
         self.model_path = model_path
         self.label_encoder_path = label_encoder_path
-            
+
     def load_tfidf(self):
         """Loads Tfidf transformer. See 
         https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.TfidfVectorizer.html
 
         for information on all functionality.
         """
-        
+
         return joblib.load(self.tfidf_path)
-    
+
     def load_model(self):
         """Loads pickled trained classifier. Current one is an XGBoost classifier.
             See: https://xgboost.readthedocs.io/en/latest/ for more details.
         """
         return joblib.load(self.model_path)
-    
+
     def load_label_encoder(self):
         """Loads label encoder. See https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.LabelEncoder.html
             for more information
 
         """
         return joblib.load(self.label_encoder_path)
-    
+
     def process_text(self, text):
         """Pre-processes input text to make it ready for prediction.
 
@@ -74,31 +115,29 @@ class ClassifierPredictor:
         Returns:
             list -- Pre-processed list of strings
         """
-        
+
         if isinstance(text, str):
-            
+
             text = [text]
-        
+
         processed_text = [i.lower().rstrip().lstrip() for i in text]
-        
+
         return processed_text
-    
-    def transform_text(self, text = None):
-        
+
+    def transform_text(self, text=None):
+
         if text is None:
             raise NoTextException("No text was given for transformation.")
-        
+
         tfidf = self.load_tfidf()
-        
+
         processed_text = self.process_text(text)
-        
+
         return tfidf.transform(processed_text)
-        
-    def predict(self, 
-                text = None, 
-                get_label_names = False,
-                predict_prob = False,
-                df_out = False):
+
+    def predict(
+        self, text=None, get_label_names=False, predict_prob=False, df_out=False
+    ):
         """Predicts origin based on classifier
 
         Keyword Arguments:
@@ -118,33 +157,41 @@ class ClassifierPredictor:
             pandas.DataFrame or list -- An object that contains predictions from the model
         """
         labels = self.load_label_encoder()
-        
+
         X = self.transform_text(text)
-        
+
         model = self.load_model()
-        
+
         if get_label_names:
             if predict_prob:
                 label_names = labels.classes_
                 probs = model.predict_proba(X)
                 result = {
-                    t : {
-                        label_name : prob for label_name, prob \
-                        in zip(label_names, probs[i])
-                        } for t, i in zip(text, range(len(probs)))
+                    t: {
+                        label_name: prob
+                        for label_name, prob in zip(label_names, probs[i])
                     }
-                
+                    for t, i in zip(text, range(len(probs)))
+                }
+
             else:
-                result = labels.inverse_transform(model.predict(X))
+                result = {
+                    name: prediction
+                    for name, prediction in zip(
+                        text, labels.inverse_transform(model.predict(X))
+                    )
+                }
         else:
             if predict_prob:
                 result = model.predict_proba(X)
             else:
                 result = model.predict(X)
-        
+
         if df_out:
-            
-            return pd.DataFrame(result).T.sort_index()
+            try:
+                return pd.DataFrame(result).T.sort_index()
+            except ValueError:
+                return pd.DataFrame(result.values(), index = result.keys()).rename({0 : 'prediction'}, axis=1)
         else:
             return result
-        
+
